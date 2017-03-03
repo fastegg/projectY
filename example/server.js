6fc11051937c09254b952c1cd8b720bf33479a15
Michael McCarry <fastegg@gmail.com>
2017-03-02 21:40:15 -0800

    Connect to mongoDB, build a table

--- example/server.js
+++ example/server.js
@@ -4,16 +4,26 @@ var projectY = require('./../index');
 var express = require('express');
 
 var app = express();
-projectY.init(function() {
-  console.log('projectY started up!');
-});
 
-//the module
-app.use('/api', projectY.use);
+var globalNewsTable = {
+  title: String,
+  description: String,
+};
+
+projectY.registerGlobalTable('news', globalNewsTable);
+
+var projConfig = {
+  mongoAddress: 'mongodb://localhost/test',
+};
+
+projectY.init(projConfig, function() {
+  //the module
+  app.use('/api', projectY.use);
+  app.listen(3000, function() {
+    console.log('Example project listening on port 3000...');
+  });
+});
 
 //serve up some static files
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('Example project listening on port 3000...');
-});
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var mongoose = require('mongoose');
+
+var Actions = require('./lib/actions');
+var Tables = require('./lib/tables');
 
 var gReady = false;
 var gStarted = false;
@@ -7,18 +11,40 @@ function isReady() {
 }
 
 //Config options: {
-// mongoServer: address to mongoDB server
-// dbName: name of mongo database
+// mongoAddress: *required* address to mongoDB server
 //}
 
 function init(config, cb) {
   if (gStarted) {
     throw 'DB already initated, do not call init twice!';
   }
-  gStarted = true;
 
-  //todo: connect to mongo and setup all tables
-  gReady = true;
+  if (!config.mongoAddress) {
+    throw 'DB address not defined!';
+  }
+
+  gStarted = true;
+  mongoose.connect(config.mongoAddress);
+  var db = mongoose.connection;
+
+  db.on('error', function(err) {
+    console.error('connection error:' + err);
+    cb(err);
+  });
+
+  db.once('open', function() {
+    db.on('disconnect', function() {
+      console.error('disconnected from DB!');
+    });
+    console.log('connected to DB');
+    gReady = true;
+    Tables.modelTables();
+    cb();
+  });
+}
+
+var API_COMMANDS = {
+  INIT_DB: 'initDB',
 }
 
 function use(req, res, next) {
@@ -26,15 +52,40 @@ function use(req, res, next) {
     throw 'DB not ready!';
   }
 
-  var path = req.path;
+  var cmd = req.path.slice(1);
+  var ctx = {};
 
-  console.log(path);
+  console.log(cmd);
 
-  res.send('asking for a request from projectY: ' + path);
-  res.end();
+  switch(cmd) {
+    case API_COMMANDS.INIT_DB:
+      Tables.initClientDB(ctx, function(err, db) {
+        if (err) {
+          res.status(400).send(err).end();
+        }
+        res.status(200).send(JSON.stringify(db ? db : {})).end();
+      });
+    break;
+    default:
+      res.status(404).send('unknown request').end();
+  }
+}
 
-  next();
+function registerTable(func, tableName, schema) {
+  console.log(func);
+  if (gStarted) {
+    throw 'cannot model a table after startup';
+  }
+
+  func(tableName, schema);
 }
 
 module.exports.init = init;
 module.exports.use = use;
+
+module.exports.registerGlobalTable = registerTable.bind(null, Tables.registerGlobalTable);
+module.exports.registerAccountTable = registerTable.bind(null, Tables.registerAccountTable);
+
+for (var cmd in Actions) {
+  module.exports[cmd] = Actions[cmd];
+}
