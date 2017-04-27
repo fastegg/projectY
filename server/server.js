56552f095ec20d7973fb99cdf8a512a94aafb786
Michael McCarry <fastegg@gmail.com>
2017-04-26 21:37:25 -0700

    added persisted sessions

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var expressFlash = require('express-flash');
 var expressSession = require('express-session');
 var loopback = require('loopback');
 var loopbackPassport = require('loopback-component-passport');
+var mongoose = require('mongoose');
+var mongoStore = require('connect-mongo')(expressSession);
 
 var app = module.exports = loopback();
 
@@ -20,19 +22,6 @@ try {
   console.trace(err);
   process.exit(1); // fatal
 }
-
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
@@ -51,14 +40,14 @@ app.middleware('auth', loopback.token({
   model: app.models.accessToken,
 }));
 
-console.log(app.get('cookieSecret'));
-
 app.middleware('session:before', cookieParser(app.get('cookieSecret')));
 app.middleware('session', expressSession({
-  secret: 'somesecretthatineedtomake',
+  secret: 'kitty',
   saveUninitialized: true,
   resave: true,
+  store: new mongoStore({url: 'mongodb://localhost:27017/projecty'}),
 }));
+
 passportConfigurator.init();
 
 // We need flash messages to see passport errors
@@ -69,6 +58,7 @@ passportConfigurator.setupModels({
   userIdentityModel: app.models.userIdentity,
   userCredentialModel: app.models.userCredential,
 });
+
 for (var s in config) {
   var c = config[s];
   c.session = c.session !== false;
@@ -76,13 +66,51 @@ for (var s in config) {
 }
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 
-app.get('/auth/account', ensureLoggedIn('/login'), function(req, res, next) {
-  res.send(JSON.stringify(req.user) + ' ' + req.url);
+var loggedInFunc = ensureLoggedIn('/login');
+
+app.get('/auth/account', function(req, res, next) {
+    loggedInFunc(req,res,next);
+  }, function(req, res, next) {
+  res.send(JSON.stringify(req.user) + ' ' + req.url + '<br/><a href="/auth/logout">Logout</a>');
 });
 
 app.get('/login', function(req, res) {
   res.send('please login in <a href="/auth/facebook">using fb</a>');
-})
+});
+
+app.post('/signup', function(req, res, next) {
+  console.log('Signup!');
+  var User = app.models.user;
+
+  var newUser = {};
+  newUser.email = req.body.email.toLowerCase();
+  newUser.username = req.body.username.trim();
+  newUser.password = req.body.password;
+
+  User.create(newUser, function(err, user) {
+    if (err) {
+      req.flash('error', err.message);
+      return res.redirect('back');
+    } else {
+      // Passport exposes a login() function on req (also aliased as logIn())
+      // that can be used to establish a login session. This function is
+      // primarily used when users sign up, during which req.login() can
+      // be invoked to log in the newly registered user.
+      req.login(user, function(err) {
+        if (err) {
+          req.flash('error', err.message);
+          return res.redirect('back');
+        }
+        return res.redirect('/auth/account');
+      });
+    }
+  });
+});
+
+app.get('/auth/logout', function(req, res, next) {
+  req.logout();
+  res.redirect('/');
+});
 
 app.start = function() {
   // start the web server
